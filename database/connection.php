a82d15759408a3cbe2f6df47b113f3d547253359
viyan-najmaldin <najmadinviyan@gmail.com>
2022-05-29 23:53:41 +0300

    ok

--- database/connection.php
+++ database/connection.php
@@ -6,6 +6,7 @@ $username = "sql5496188";
 $pass ="L5ztG5lldr";
 $db= null;
 
+
 try{
   $db= new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $pass,[
     PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION
